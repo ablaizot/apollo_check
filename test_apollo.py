0697f8763a420a4ec3108fd6135c1622850aed01
ablaizot <ablaizot@bu.edu>
2023-02-27 15:39:54 -0500

    using Popen to pass commands to apollo

--- test_apollo.py
+++ test_apollo.py
@@ -9,17 +9,27 @@ def main():
 
     [host_list, out_path] = apollo_def.validate_connections()
 
-    
+    commands = ['ls -al', 'systemctl --failed']
 
     for HOST in host_list:
         try:
             
             # Set the ssh command with timeout of 10 seconds
-            ssh_command = f"ssh -o ConnectTimeout=10 cms@{HOST} systemctl --failed"
-            print(subprocess.run([ssh_command],shell=True))
+            ssh_cmd = ['ssh', f'cms@{HOST}']
+            
+            #print(subprocess.run([ssh_command],shell=True))
             # Use subprocess to run the command
             #ssh_proc = subprocess.Popen(ssh_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            
+            ssh_session = subprocess.Popen(ssh_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            for command in commands:
+                ssh_session.stdin.write(command.encode())
+                ssh_session.stdin.write(b'\n')
+            ssh_session.stdin.close()
+            for line in ssh_session.stdout:
+                print(line.decode().rstrip())
+            for line in ssh_session.stderr:
+                print(line.decode().rstrip())
+            ssh_session.wait()
             # Wait for the command to finish or timeout
             #stdout, stderr = ssh_proc.communicate(timeout=10)
             
