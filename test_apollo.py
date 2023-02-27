2bc166f7a9aef9e2179add48db943c994033ab8d
“ablaizot” <ablaizot@bu.edu>
2023-02-27 11:38:24 -0500

    added formatting

--- test_apollo.py
+++ test_apollo.py
@@ -13,9 +13,10 @@ def main():
 
     for HOST in host_list:
         try:
+            
             # Set the ssh command with timeout of 10 seconds
-            ssh_command = "ssh -o ConnectTimeout=10 {HOST}"
-            print(subprocess.run(["ssh -o ConnectTimeout=10 {HOST}"],shell=True))
+            ssh_command = f"ssh -o ConnectTimeout=10 {HOST}"
+            print(subprocess.run([f"ssh -o ConnectTimeout=10 {HOST}"],shell=True))
             print(subprocess.run(["systemctl --failed"],shell=True))
             # Use subprocess to run the command
             #ssh_proc = subprocess.Popen(ssh_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
